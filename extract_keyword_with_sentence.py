0916304d2cf8bd4f1131b0ec16bc11108dbd4ac8
AliyaArystanbek <83461361+AliyaArystanbek@users.noreply.github.com>
2021-11-17 13:55:59 +0100

    Add files via upload

---
+++ extract_keyword_with_sentence.py
@@ -0,0 +1,39 @@
+import urllib.request
+import re
+import bs4 as bs
+import nltk
+from nltk import sent_tokenize, word_tokenize
+from nltk.corpus import stopwords
+
+
+
+url = "https://www.japan-zone.com/culture/food.shtml"
+html = urllib.request.urlopen(url)
+# parsing the html file
+htmlParse = bs.BeautifulSoup(html, 'html.parser')
+
+# getting all the paragraphs
+fileinF = []
+for para in htmlParse.find_all("p"):
+
+    fileinE = para.get_text()
+    #print(fileinE)
+    li = list(fileinE.split("."))
+    keywords = ['japanese']
+    #print(li)
+
+
+
+    for sent in li:
+        tokenized_sent = [word.lower() for word in word_tokenize(sent)]
+
+        if any(keyw in tokenized_sent for keyw in keywords):
+            fileinF.append(sent)
+            
+list_with_keyword = fileinF
+#print(list_with_keyword)
+text_with_keyword = '. '.join([str(elem) for elem in list_with_keyword])
+print(text_with_keyword)
+
+
+       
